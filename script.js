ad5787b5ac780d3a4e9b2f2ac8259b1e2b30868f
Sincal <sincaljulio200@gmail.com>
2024-04-02 14:47:35 -0600

    SCRUM-11 <Constantes js>

---
+++ script.js
@@ -0,0 +1,8 @@
+const timeElement = document.getElementById('time');
+const dateElement = document.getElementById('date');
+const currentWeatherItemsElement = document.getElementById
+('current-weather-items');
+const timeZone = document.getElementById('time-zone');
+const countryElement = document.getElementById('country');
+const weatherForecastElement = document.getElementById('weather-forecast');
+const currentTempElement = document.getElementById('current-temp');
