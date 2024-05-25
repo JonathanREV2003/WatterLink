eb027704a03ca5feb4aace748cc0b8d263ff97d5
Sincal <50584963+Sincal200@users.noreply.github.com>
2024-05-24 19:34:26 -0600

    Scrum 37 cambio estetico general (#11)
    * SCRUM-37<visibility password>
    
    usuario hará visible su contraseña en caso de amnesia temporal
    
    * Update index.html
    
    * Revert "Update index.html"
    
    This reverts commit fc1e09ac373c62ad8b3182c791a5dfbcc54ef678.
    
    * SCRUM-37<link_icon>
    
    Enlace a distintas redes sociales de la empresa
    
    ---------
    
    Co-authored-by: GustaCortez <melvincortey@gmail.com>

--- loginScrip.js
+++ loginScrip.js
@@ -9,3 +9,15 @@ document.addEventListener('click', e => {
         $signUp.classList.toggle('active')
     }
 });
+
+const togglePasswordButtons = document.querySelectorAll('.toggle-password');
+
+togglePasswordButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const passwordInput = button.previousElementSibling;
+    const isPasswordVisible = passwordInput.type === 'text';
+
+    passwordInput.type = isPasswordVisible ? 'password' : 'text';
+    button.classList.toggle('active');
+  });
+});
